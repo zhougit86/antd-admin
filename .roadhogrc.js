f079e5a42da89e11686c07dec5792cc31338d310
chenkang1 <chenkang1@lenovo.com>
2017-06-25 17:22:51 +0800

    add dll

--- .roadhogrc.js
+++ .roadhogrc.js
@@ -10,20 +10,30 @@ export default {
   svgSpriteLoaderDirs: svgSpriteDirs,
   "theme": "./theme.config.js",
   "env": {
-      "development": {
-        "extraBabelPlugins": [
-          "dva-hmr",
-          "transform-runtime",
-  		    ["import", { "libraryName": "antd", "style": true }]
-        ],
-        "devtool":'cheap-module-source-map',
-        "port":4000
-      },
-      "production": {
-        "extraBabelPlugins": [
-          "transform-runtime",
-  		    ["import", { "libraryName": "antd", "style": true}]
-        ]
-      }
+    "development": {
+      "extraBabelPlugins": [
+        "dva-hmr",
+        "transform-runtime",
+        ["import", {"libraryName": "antd", "style": true}]
+      ],
+      "devtool": 'cheap-module-source-map',
+      "port": 4000
+    },
+    "production": {
+      "extraBabelPlugins": [
+        "transform-runtime",
+        ["import", {"libraryName": "antd", "style": true}]
+      ]
+    }
+  },
+  "dllPlugin": {
+    "exclude": [
+      "babel-runtime"
+    ],
+    "include": [
+      "dva/router",
+      "dva/saga",
+      "dva/fetch"
+    ]
   }
 }
