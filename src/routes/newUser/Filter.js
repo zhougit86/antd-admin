062a7b5ce935cfbcb8b8ee7b7a4c59a0ed763189
zhou xiaogang <zhouxg2@lenovo.com>
2017-06-27 18:16:14 +0800

    filter ongoing

--- src/routes/newUser/Filter.js
+++ src/routes/newUser/Filter.js
@@ -3,3 +3,41 @@
  */
 
 
+import React from 'react'
+import PropTypes from 'prop-types'
+import { FilterItem } from '../../components'
+import { Form, Row, Col, Input } from 'antd'
+
+const Search = Input.Search
+const ColProps = {
+  xs: 24,
+  sm: 12,
+  style: {
+    marginBottom: 16,
+  },
+}
+
+
+const Filter = ({
+  onFilterChange,
+  name,
+  form: {
+    getFieldDecorator,
+    getFieldsValue,
+  },
+})=>{
+  const handleSubmit = () => {
+    let fields = getFieldsValue()
+    onFilterChange(fields)
+  };
+  return (
+    <Row gutter={24}>
+      <Col {...ColProps} xl={{ span: 4 }} md={{ span: 8 }}>
+        {getFieldDecorator('name', { initialValue: name })(<Search placeholder="Search Name" size="large" onSearch={handleSubmit} />)}
+      </Col>
+    </Row>
+  )
+}
+
+
+export default Filter
